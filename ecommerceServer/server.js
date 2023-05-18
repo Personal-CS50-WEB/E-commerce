e12e86af6d07cfc97cddb0498924b589c617537b
Olamakhlouf <olamoatasem@gmail.com>
2023-05-18 10:16:59 -0300

    productRouter

--- ecommerceServer/server.js
+++ ecommerceServer/server.js
@@ -6,19 +6,48 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const connectDB = require('./src/configs/connect-db')
 
-const { connectAndCreateCollections } = require('./src/models/createCollections.js');
-
-const PORT = process.env.PORT || 4001;
+const connectAndCreateCollections = require('./src/models/createCollections.js');
 const app = express();
 
-app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname, '/public/')));
 
-app.get('/', (req, res) => {
-    res.send('Hello from my app');
+async function startServer() {
+    try {
+        const db = await connectAndCreateCollections();
+        let apiRouter = require("./src/routes/api")(db);
+        // Mount the API router
+        app.use('/api', apiRouter);
+
+        // Start the server
+        const PORT = process.env.PORT || 4001;
+
+        app.use(morgan('tiny'));
+        app.use(express.static(path.join(__dirname, '/public/')));
+
+        app.get('/', (req, res) => {
+            res.send('Hello from my app');
+        });
+        app.use(
+            cors(),
+            bodyParser.urlencoded({ extended: true }),
+            bodyParser.json()
+        );
+        app.listen(PORT, () => {
+            debug(`listening on port ${chalk.green(PORT)}`);
+        });
+    } catch (error) {
+        console.error('Failed to connect to MongoDB', error);
+    }
+}
+
+// Close the MongoDB connection when the app is terminated
+process.on('SIGINT', async () => {
+    await connectDB.close();
+    process.exit();
 });
 
-app.listen(PORT, () => {
-    debug(`listening on port ${chalk.green(PORT)}`);
-});
+// Start the server
+startServer();
